ec99ec2852673db34c53f419f0b3a7f3116d1bce
dsamwald <dsamwald@student.tgm.ac.at>
2017-03-30 13:39:41 +0200

    Zweite Version

--- samwald/Test0330.java
+++ samwald/Test0330.java
@@ -3,5 +3,9 @@ package samwald;
 public class Test0330 {
 	public static void main(String[] args){
 		System.out.println("Guten Tag Hr. Samwald!!!!");
+		System.out.println("Guten Tag Hr. Samwald!!!!");
+		System.out.println("Guten Tag Hr. Samwald!!!!");
+		System.out.println("Guten Tag Hr. Samwald!!!!");
+		System.out.println("Guten Tag Hr. Samwald!!!!");
 	}
 }
