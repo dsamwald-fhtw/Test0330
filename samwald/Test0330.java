391cc541accb220ae9234c597d7ab553820d3c9b
dsamwald <dsamwald@student.tgm.ac.at>
2017-03-30 13:42:04 +0200

    English version

--- samwald/Test0330.java
+++ samwald/Test0330.java
@@ -2,6 +2,6 @@ package samwald;
 
 public class Test0330 {
 	public static void main(String[] args){
-		System.out.println("Guten Tag Hr. Samwald!!!!");
+		System.out.println("Good morning mr. Samwald!!!!");
 	}
 }
